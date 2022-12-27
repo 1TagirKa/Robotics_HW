d6e1adbc6700b5e590afe0d2933f662d5e93897d
1TagirKa <80746101+1TagirKa@users.noreply.github.com>
2022-12-27 10:55:39 +0300

    Homework 8_2

--- Number8/Number8_2.py
+++ Number8/Number8_2.py
@@ -1,58 +1,30 @@
 import cv2
-import os
 import numpy as np
-import matplotlib.pyplot as plt
 
-img = cv2.imread('../Picture/1556708032_1.jpg')
-scale_up_x = 0.5
-scale_up_y = 0.5
-scale_down = 0.6
-width = int(img.shape[1] * scale_up_x)
-height = int(img.shape[0] * scale_up_y)
-dim = (width, height)
-a = res_inter_nearset = cv2.resize(img, dim, interpolation = cv2.INTER_NEAREST)
-b = res_inter_linear = cv2.resize(img, dim, interpolation = cv2.INTER_LINEAR)
-c = res_inter_area = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-print(a)
-print(b)
-print(c)
+points = np.zeros((100, 2), np.int32)
+count = 0
+distPix = 0
 
-cv2.imshow('INTER_NEARES',a)
-cv2.waitKey()
+def draw_line(event, x, y, flags, params):
+    global mouseX, mouseY, count
+    if event == cv2.EVENT_LBUTTONDOWN:
+        cv2.circle(map_route, (x, y), 3, (255, 0, 0), -1)
+        points[count] = x, y
+        count += 1
+        print(x,y)
+        if count >= 2:
+            cv2.line(map_route, points[count - 2], points[count - 1], (0, 0, 0), thickness=3, lineType=cv2.LINE_AA)
 
-cv2.imshow('INTER_LINEAR',b)
-cv2.waitKey()
 
-cv2.imshow('INTER_AREA',c)
-cv2.waitKey()
+map_route = cv2.imread('map.png')
+
+while True:
+
+    cv2.imshow('map', map_route)
+    cv2.setMouseCallback('map', draw_line)
+
+    if cv2.waitKey(1) & 0xFF == 27:
+        if count >= 2:
+            cv2.imwrite('resultMap.png', map_route)
+        break
 cv2.destroyAllWindows()
-
-size = img.shape
-print(size)
-
-columns = 3
-rows = 3
-
-y_patch_length = size[0] / 3
-x_patch_length = size[1] / 3
-for i in range(0, rows):
-    for j in range(0, columns):
-        int_x_patch = int(x_patch_length)
-        int_y_patch = int(y_patch_length)
-        cropped_x = int_x_patch * j
-        cropped_x1 = int_x_patch * (j+1)
-        cropped_y = int_y_patch * i
-        cropped_y1 = int_y_patch * (i+1)
-        patch = img[cropped_y : cropped_y1, cropped_x : cropped_x1]
-        cv2.imwrite('../Picture/patch('+ str(i) +':'+ str(j) + '.png', patch)
-
-import matplotlib.pyplot as plt
-
-fig1, fig2 = plt.subplots(nrows=rows, ncols=columns)
-for i in range(0, rows):
-    for j in range(0, columns):
-        img = plt.imread('../Picture/patch('+ str(i) +':'+ str(j) + '.png')
-        fig2[i,j].axis('off')
-        fig2[i,j].imshow(img)
-plt.tight_layout()
-plt.show()
